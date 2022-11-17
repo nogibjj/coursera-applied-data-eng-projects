070c88144665e6c26b36df178d495d396d9fa985
Noah Gift <noah.gift@gmail.com>
2022-11-17 18:37:55 +0000

    adding finished example

--- main.py
+++ main.py
@@ -1,4 +1,55 @@
+#!/usr/bin/env python3
+
 """
 Main cli or app entry point
 """
+import yake
+import click
 
+# create a function that reads a file
+def read_file(filename):
+    with open(filename, encoding="utf-8") as myfile:
+        return myfile.read()
+
+
+# def extract keywords
+def extract_keywords(text):
+    kw_extractor = yake.KeywordExtractor()
+    keywords = kw_extractor.extract_keywords(text)
+    return keywords
+
+
+# create a function that makes hash tags
+def make_hashtags(keywords):
+    hashtags = []
+    for keyword in keywords:
+        hashtags.append("#" + keyword[0].replace(" ", ""))
+    return hashtags
+
+
+@click.group()
+def cli():
+    """A cli for keyword extraction"""
+
+
+@cli.command("extract")
+@click.argument("filename")
+def extract(filename):
+    """Extract keywords from a file"""
+    text = read_file(filename)
+    keywords = extract_keywords(text)
+    click.echo(keywords)
+
+
+@cli.command("hashtags")
+@click.argument("filename")
+def hashtagscli(filename):
+    """Extract keywords from a file and make hashtags"""
+    text = read_file(filename)
+    keywords = extract_keywords(text)
+    hashtags = make_hashtags(keywords)
+    click.echo(hashtags)
+
+
+if __name__ == "__main__":
+    cli()
--- test_main.py
+++ test_main.py
@@ -2,4 +2,32 @@
 Test goes here
 
 """
+from main import read_file, extract_keywords, make_hashtags
+from click.testing import CliRunner
+from main import cli
 
+
+def test_read_file():
+    text = read_file("text.txt")
+    assert "hugging" in text
+
+
+def test_extract_keywords():
+    text = read_file("text.txt")
+    keywords = extract_keywords(text)
+    assert len(keywords) > 0
+
+
+def test_make_hashtags():
+    text = read_file("text.txt")
+    keywords = extract_keywords(text)
+    hashtags = make_hashtags(keywords)
+    assert len(hashtags) > 0
+    assert "#" in hashtags[0]
+
+
+def test_cli_extract():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["extract", "text.txt"])
+    assert result.exit_code == 0
+    assert "hugging" in result.output
